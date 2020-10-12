17e176509886421a414490e7c9a59dd52502c15c
pochi-gh <pochigm@gmail.com>
2020-10-12 14:47:25 +0900

    メインページの修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,14 +3,21 @@
 @section('content')
     <body>
     <h1>お店一覧</h1>
+      <table class='table table-striped table-hover'>
+        <tr>
+            <th>カテゴリ</th><th>店名</th><th>住所</th>
+        </tr>
+        @foreach ($shops as $shop)
+          <tr>
+            <td>{{ $shop->category->name }}</td>
+            <td>
+              <a href={{route('shop.show',['id' => $shop->id])}}>{{ $shop->name }}</a>
+            </td>
+            <td>{{ $shop->address }}</td>
 
-    @foreach ($shops as $shop)
-        <p>
-        {{ $shop->category->name }},
-        {{ $shop->name }},
-        {{ $shop->address }}
-        </p>
-    @endforeach
+          </tr>
+
+        @endforeach
     </body>
 @endsection
 
